3d0feb0aadfb1ab581e717841f2820aefe764307
UFO <627375202@qq.com>
2022-05-13 16:48:49 +0800

    test init

---
+++ libs/src/main/java/com/dw/libs/utils/DWUtil.java
@@ -0,0 +1,17 @@
+package com.dw.libs.utils;
+
+import android.util.Log;
+
+/**
+ * 日期：2022/5/13
+ * <p>
+ * 作者：xudiwei
+ * <p>
+ * 描述：
+ */
+public class DWUtil {
+
+    public static void show(){
+        Log.d("DWUtil","DW INOF");
+    }
+}
